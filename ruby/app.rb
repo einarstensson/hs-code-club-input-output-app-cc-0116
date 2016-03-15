0d7857c58103babfc057bf8564c5ee4778f09bb5
einarstensson <einar.stensson@gmail.com>
2016-03-15 15:21:02 -0400

    Done.

---
+++ ruby/app.rb
@@ -0,0 +1,10 @@
+puts "Welcome to the most amazing pizza store ever"
+puts "What would you like to order?"
+order = gets.chomp
+
+puts "How many would you like?"
+quantity = gets.chomp.to_i
+
+total = 18 * quantity
+
+puts "Your total is $" + total.to_s
